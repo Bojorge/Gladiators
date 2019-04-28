747e80507cc475656a7bb5b50bd08ec4e86b0701
Bojorge <38023924+Bojorge@users.noreply.github.com>
2019-04-27 22:17:15 -0600

    Repositorio creado

---
+++ Gladiators/cmake-build-debug/CMakeFiles/Gladiators.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Gladiators.dir/main.cpp.o"
+  "Gladiators.pdb"
+  "Gladiators"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Gladiators.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
