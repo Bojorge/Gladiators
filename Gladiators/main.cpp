a1cfc8c80b5e703fcaec064fe86aa9ae4016b5eb
Bojorge <38023924+Bojorge@users.noreply.github.com>
2019-04-29 00:36:59 -0600

    Algoritmo A*

--- Gladiators/main.cpp
+++ Gladiators/main.cpp
@@ -1,6 +1,32 @@
-#include <iostream>
+#include "AStar.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main()
+{
+    typedef pair<int, int> Pair;
+    AStar pathfinding;
+    /* Descripcion de la cuadricula
+    1--> La celda esta habilitada
+    0--> La celda esta bloqueada */
+    int grid[FILA][COLUMNA] =
+            {
+                    { 1, 0, 1, 1, 1, 1, 0, 1, 1, 1 },
+                    { 1, 1, 1, 0, 1, 1, 1, 0, 1, 1 },
+                    { 1, 1, 1, 0, 1, 1, 0, 1, 0, 1 },
+                    { 0, 0, 1, 0, 1, 0, 0, 0, 0, 1 },
+                    { 1, 1, 1, 0, 1, 1, 1, 0, 1, 0 },
+                    { 1, 0, 1, 1, 1, 1, 0, 1, 0, 0 },
+                    { 1, 0, 0, 0, 0, 1, 0, 0, 0, 1 },
+                    { 1, 0, 1, 1, 1, 1, 0, 1, 1, 1 },
+                    { 1, 1, 1, 0, 0, 0, 1, 0, 0, 1 }
+            };
+
+    // Source is the left-most bottom-most corner
+    Pair inicio = make_pair(8, 0);
+
+    // Destination is the left-most top-most corner
+    Pair destino = make_pair(0, 0);
+
+    pathfinding.aStarSearch(grid, inicio, destino);
+
+    return(0);
 }
