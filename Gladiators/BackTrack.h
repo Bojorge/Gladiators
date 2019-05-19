5893bb1907e8a17ce13c90c91f8f0e6a54ffaf2c
Bojorge <38023924+Bojorge@users.noreply.github.com>
2019-05-19 00:58:39 -0600

    Ahora si funciona el BackTracking

--- Gladiators/BackTrack.h
+++ Gladiators/BackTrack.h
@@ -35,54 +35,130 @@ public:
         backtrack.anadir_final(actualX);
         backtrack.anadir_final(actualY);
 
-        while(actualY != finalY && actualX != finalX) {
-            if (grid[actualY + 1][actualX + 1] == 0) {
-                actualX += 1;
-                actualY += 1;
-                camino.anadir_final(actualX);
-                camino.anadir_final(actualY);
-                backtrack.anadir_final(actualX);
-                backtrack.anadir_final(actualY);
-                cout << "se mueve en diagonal \n";
-                sleep(1);
-            } else if (grid[actualY][actualX + 1] == 0) {
-                actualX += 1;
-                camino.anadir_final(actualX);
-                camino.anadir_final(actualY);
-                backtrack.anadir_final(actualX);
-                backtrack.anadir_final(actualY);
-                cout << "se mueve hacia la derecha \n";
-                sleep(1);
-            } else if (grid[actualY + 1][actualX] == 0) {
-                actualY += 1;
-                camino.anadir_final(actualX);
-                camino.anadir_final(actualY);
-                backtrack.anadir_final(actualX);
-                backtrack.anadir_final(actualY);
-                cout << "se mueve hacia abajo \n";
-                sleep(1);
+        while(actualY <= finalY && actualX <= finalX) {
+
+            if (actualY==finalY && actualX==finalX){
+                cout << "\n***  El gladiador llego a su destino  ***  >>> ("<<actualX<<", "<<actualY<<")\n";
+                camino.print_lista();
+                return camino;
             }
-            else{
-                actualX=backtrack.obtener_dato(backtrack.tamano()-4);
-                actualY=backtrack.obtener_dato(backtrack.tamano()-3);
+            else if (actualY==finalY && actualX<finalX){
+                if(grid[actualY][actualX+1]==0){
+                    actualX+=1;
+                    camino.anadir_final(actualX);
+                    camino.anadir_final(actualY);
+                    backtrack.anadir_final(actualX);
+                    backtrack.anadir_final(actualY);
+                    cout << "\n llego al borde inferior pero no ha llegado al objetivo  ***  se movera hacia la derecha  ***  >>> ("<<actualX<<", "<<actualY<<")\n";
+                    sleep(1);
+                } else if(grid[actualY][actualX+1]!=0){
+                    while(grid[actualY][actualX+1]!=0){
+                        if(actualY==0){
+                            cout << "\n -----  no hay camino  ----- \n";
+                            sleep(1);
+                            return camino;
+                        } else if (grid[actualY-1][actualX+1] == 0) {
+                            actualY-=1;
+                            actualX+=1;
+                            camino.anadir_final(actualX);
+                            camino.anadir_final(actualY);
+                            backtrack.anadir_final(actualX);
+                            backtrack.anadir_final(actualY);
+                            cout << "\n se mueve en diagonal hacia arriba y hacia la derecha  >>> ("<<actualX<<", "<<actualY<<")\n";
+                            sleep(1);
+                        } else if (grid[actualY-1][actualX] == 0) {
+                            actualY -= 1;
+                            camino.anadir_final(actualX);
+                            camino.anadir_final(actualY);
+                            backtrack.anadir_final(actualX);
+                            backtrack.anadir_final(actualY);
+                            cout << "\n se mueve hacia arriba   >>> ("<<actualX<<", "<<actualY<<")\n";
+                            sleep(1);
+                        }
+                    }
+                }
+            } else if (actualX==finalX && actualY<finalY){
+                if(grid[actualY+1][actualX]==0){
+                    actualY+=1;
+                    camino.anadir_final(actualX);
+                    camino.anadir_final(actualY);
+                    backtrack.anadir_final(actualX);
+                    backtrack.anadir_final(actualY);
+                    cout << "\n llego al borde derecho pero no ha llegado al objetivo  ***  se movera hacia abajo  ***   >>> ("<<actualX<<", "<<actualY<<")\n";
+                    sleep(1);
+                } else if(grid[actualY+1][actualX]!=0){
+                    while(grid[actualY+1][actualX]!=0){
+                        if(actualX==0){
+                            cout << "\n -----  no hay camino  ----- \n";
+                            sleep(1);
+                            return camino;
+                        } else if (grid[actualY+1][actualX-1] == 0) {
+                            actualY+=1;
+                            actualX-=1;
+                            camino.anadir_final(actualX);
+                            camino.anadir_final(actualY);
+                            backtrack.anadir_final(actualX);
+                            backtrack.anadir_final(actualY);
+                            cout << "\n se mueve en diagonal hacia abajo y hacia la izquierda   >>> ("<<actualX<<", "<<actualY<<")\n";
+                            sleep(1);
+                        } else if (grid[actualY][actualX-1] == 0) {
+                            actualX -= 1;
+                            camino.anadir_final(actualX);
+                            camino.anadir_final(actualY);
+                            backtrack.anadir_final(actualX);
+                            backtrack.anadir_final(actualY);
+                            cout << "\n se mueve hacia la izquierda   >>> ("<<actualX<<", "<<actualY<<")\n";
+                            sleep(1);
+                        }
+                    }
+                }
+            } else if(actualY<finalY && actualX<finalX) {
+                if (grid[actualY + 1][actualX + 1] == 0) {
+                    actualX += 1;
+                    actualY += 1;
+                    camino.anadir_final(actualX);
+                    camino.anadir_final(actualY);
+                    backtrack.anadir_final(actualX);
+                    backtrack.anadir_final(actualY);
+                    cout << "\n se mueve en diagonal hacia abajo y hacia la derecha   >>> ("<<actualX<<", "<<actualY<<")\n";
+                    sleep(1);
+                } else if (grid[actualY][actualX + 1] == 0) {
+                    actualX += 1;
+                    camino.anadir_final(actualX);
+                    camino.anadir_final(actualY);
+                    backtrack.anadir_final(actualX);
+                    backtrack.anadir_final(actualY);
+                    cout << "\n se mueve hacia la derecha   >>> ("<<actualX<<", "<<actualY<<")\n";
+                    sleep(1);
+                } else if (grid[actualY + 1][actualX] == 0) {
+                    actualY += 1;
+                    camino.anadir_final(actualX);
+                    camino.anadir_final(actualY);
+                    backtrack.anadir_final(actualX);
+                    backtrack.anadir_final(actualY);
+                    cout << "\n se mueve hacia abajo   >>> ("<<actualX<<", "<<actualY<<")\n";
+                    sleep(1);
+                } else{
+                    actualX=backtrack.obtener_dato(backtrack.tamano()-4);
+                    actualY=backtrack.obtener_dato(backtrack.tamano()-3);
 
-                camino.anadir_final(actualX);
-                camino.anadir_final(actualY);
+                    camino.anadir_final(actualX);
+                    camino.anadir_final(actualY);
 
-                grid[backtrack.obtener_dato(backtrack.tamano()-1)][backtrack.obtener_dato(backtrack.tamano()-2)]=1;
+                    grid[backtrack.obtener_dato(backtrack.tamano()-1)][backtrack.obtener_dato(backtrack.tamano()-2)]=1;
 
-                backtrack.borrar_Dato((backtrack.tamano()-1));
-                backtrack.borrar_Dato((backtrack.tamano()-1));
+                    backtrack.borrar_Dato((backtrack.tamano()-1));
+                    backtrack.borrar_Dato((backtrack.tamano()-1));
 
-                cout << "se devuelve porque el camino esta bloqueado \n";
-                sleep(1);
+                    cout << "\n se devuelve porque el camino esta bloqueado   >>> ("<<actualX<<", "<<actualY<<")\n";
+                    sleep(1);
 
+                }
             }
+
 
 
         }
-        cout << "\n***  El gladiador llego a su destino  ***\n";
-        camino.print_lista();
         return camino;
 
 
